23e2d415d6e90daafecd07a472b2ab5d921c013e
Taohid0 <taohidulii@gmail.com>
2018-02-09 21:26:10 +0600

    Bresenham's line algorithm added

---
+++ bresenhams_line_algorithm.py
@@ -0,0 +1,30 @@
+from graphics import *
+import time
+
+x1, y1 = map(int,input("Starting point : ").split())
+x2, y2 = map(int,input("Ending point : ").split())
+x = x1
+y = y1
+dx = x2-x1
+dy = y2-y1
+dt = 2*(dy-dx)
+ds = 2*dy
+d = 2*dy-dx
+win = GraphWin("Bresenham's line algorithm", 1000, 800)
+win.plotPixel(x, y)
+
+while (x<x2):
+    x = x+1
+
+    if( d<0 ):
+        d = d+ds
+    else:
+        y = y+1
+        d = d+dt
+    win.plotPixel(x, y)
+    time.sleep(.02)
+
+try:
+    win.getMouse()
+except:
+    pass
